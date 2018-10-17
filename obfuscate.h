f214f94b3cd7de2b4cbdeb32387b110d6dcad7b4
Adam Yaxley <craincat666@gmail.com>
2018-10-17 09:58:18 +0900

    Add header and license to obfuscate.h file

--- obfuscate.h
+++ obfuscate.h
@@ -1,3 +1,23 @@
+/* --------------------------- ABOUT ---------------------------------
+
+Original Author: Adam Yaxley
+Website: https://github.com/adamyaxley
+License: See end of file
+
+Obfuscate
+Guaranteed compile-time string literal obfuscation library for C++14
+
+Usage:
+Pass string literals into the AY_OBFUSCATE macro to obfuscate them at
+compile time. AY_OBFUSCATE returns a temporary ay::obfuscated_data
+object that is implicitly convertable to a char*.
+
+Example:
+auto obfuscated_string = AY_OBFUSCATE("Hello World");
+std::cout << obfuscated_string << std::endl;
+
+------------------------------------------------------------------- */
+
 #include <cstddef>
 
 namespace ay
@@ -125,3 +145,32 @@ namespace ay
 		constexpr auto obfuscator = ay::makeObfuscator<n, key>(data); \
 		return ay::obfuscated_data<n, key>(obfuscator); \
 	}()
+	
+/* --------------------------- LICENSE -------------------------------
+
+Public Domain (http://www.unlicense.org)
+
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+------------------------------------------------------------------- */
