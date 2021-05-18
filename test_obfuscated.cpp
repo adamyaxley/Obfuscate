e6213e83080ff4e1595189d3485cfa45d4e770ba
Adam Yaxley <craincat666@gmail.com>
2021-05-18 09:09:12 +0900

    Give each failure case a unique id to avoid overlapping checks (eg char and const char)

--- test_obfuscated.cpp
+++ test_obfuscated.cpp
@@ -5,23 +5,23 @@
 int main()
 {
 	{
-		auto str = AY_OBFUSCATE("auto str");
+		auto str = AY_OBFUSCATE("1 auto str");
 		puts(str);
 	}
 	{
-		char* str = AY_OBFUSCATE("char* str");
+		char* str = AY_OBFUSCATE("2 char* str");
 		puts(str);
 	}
 	{
-		const char* str = "const char* str"; // test fail case temporarily
+		const char* str = "3 const char* str"; // test fail case temporarily
 		puts(str);
 	}
 	{
-		static const char* str = AY_OBFUSCATE("static const char* str");
+		static const char* str = AY_OBFUSCATE("4 static const char* str");
 		puts(str);
 	}
 	{
-		auto str = AY_OBFUSCATE("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed ullamcorper lectus. Cras dapibus, turpis in dignissim consequat, justo massa vehicula sem, vestibulum condimentum dui risus vitae nunc. Mauris tincidunt condimentum nulla, non elementum lectus molestie id. Ut eget libero lorem. Proin vitae massa vehicula, hendrerit est a, tincidunt est. Sed aliquam velit quam, laoreet commodo massa aliquam et. Phasellus in nulla ac risus facilisis porttitor sit amet at enim. Vivamus ornare libero sit amet enim egestas semper vel non dolor.Ut vitae scelerisque elit, sed tincidunt tellus.Maecenas a accumsan justo, sit amet auctor nisl.Donec et quam mollis.");
+		auto str = AY_OBFUSCATE("5 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed ullamcorper lectus. Cras dapibus, turpis in dignissim consequat, justo massa vehicula sem, vestibulum condimentum dui risus vitae nunc. Mauris tincidunt condimentum nulla, non elementum lectus molestie id. Ut eget libero lorem. Proin vitae massa vehicula, hendrerit est a, tincidunt est. Sed aliquam velit quam, laoreet commodo massa aliquam et. Phasellus in nulla ac risus facilisis porttitor sit amet at enim. Vivamus ornare libero sit amet enim egestas semper vel non dolor.Ut vitae scelerisque elit, sed tincidunt tellus.Maecenas a accumsan justo, sit amet auctor nisl.Donec et quam mollis.");
 		puts(str);
 	}
 
